c184d07088fe68f1a087c5828543d4a3f16fac21
Tejas Arlimatti <tejasarlimatti@gmail.com>
2020-12-26 12:55:17 +0530

    Added a `logger` package. 1. Added a `Printf` function to help with integrating with the ES Client's logger

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/tejasa97/utils-go
 
 go 1.15
 
-require github.com/stretchr/testify v1.6.1
+require (
+	github.com/stretchr/testify v1.6.1
+	go.uber.org/zap v1.16.0
+)
---
+++ logger/logger.go
@@ -0,0 +1,65 @@
+package logger
+
+import (
+	"fmt"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+var (
+	log logger
+)
+
+type logger struct {
+	log *zap.Logger
+}
+
+func init() {
+	logConfig := zap.Config{
+		OutputPaths: []string{"stdout"},
+		Level:       zap.NewAtomicLevelAt(zap.InfoLevel),
+		Encoding:    "json",
+		EncoderConfig: zapcore.EncoderConfig{
+			LevelKey:     "level",
+			TimeKey:      "time",
+			MessageKey:   "msg",
+			EncodeTime:   zapcore.ISO8601TimeEncoder,
+			EncodeLevel:  zapcore.LowercaseLevelEncoder,
+			EncodeCaller: zapcore.ShortCallerEncoder,
+		},
+	}
+
+	var err error
+	if log.log, err = logConfig.Build(); err != nil {
+		panic(err)
+	}
+}
+
+func GetLogger() logger {
+	return log
+}
+
+func (l logger) Printf(format string, v ...interface{}) {
+	if len(v) == 0 {
+		Info(format)
+	} else {
+		Info(fmt.Sprintf(format, v...))
+	}
+}
+
+func Info(msg string, tags ...zap.Field) {
+	log.log.Info(msg, tags...)
+	log.log.Sync()
+}
+
+func Debug(msg string, tags ...zap.Field) {
+	log.log.Debug(msg, tags...)
+	log.log.Sync()
+}
+
+func Error(msg string, err error, tags ...zap.Field) {
+	tags = append(tags, zap.NamedError("error", err))
+	log.log.Error(msg, tags...)
+	log.log.Sync()
+}
